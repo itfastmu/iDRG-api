b1914534d1f089cf732a443d15d2c54c9a408f5a
xmath06 <xmail.black@gmail.com>
2025-09-26 19:53:10 +0700

    trying respon url eklaim

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Elysia } from "elysia";
 import post from "./post";
 import get from "./get";
 import cors from "@elysiajs/cors";
+const mode = Bun.env.MODE === "debug" ? "?mode=debug" : "";
 
 const app = new Elysia()
     .use(cors())
     .get("/", () => "Hello Elysia")
+    .get("/coba", () => {
+        return Bun.env.EKLAIM_URL + mode;
+    })
     .use(post)
     .use(get)
     .listen(3000);
