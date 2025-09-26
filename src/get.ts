e96626a9418618ccd113b69034545c65ddde7942
xmath06 <xmail.black@gmail.com>
2025-09-26 08:02:18 +0700

    connecting db idrg

--- src/get.ts
+++ src/get.ts
@@ -13,9 +13,9 @@ const get = new Elysia({ prefix: '/grab' })
                     tanggal += ` AND reg_periksa.status_lanjut = 'Ranap'`;
                 }
             }
-            const fields = "reg_periksa.no_rawat, reg_periksa.no_rkm_medis, reg_periksa.tgl_registrasi, case when status_lanjut = 'Ralan' then 'Rawat Jalan' else 'Rawat Inap' end as status, bridging_sep.no_sep, bridging_sep.no_kartu, bridging_sep.klsrawat, pasien.nm_pasien, pasien.tgl_lahir, pasien.jk";
+            const fields = "reg_periksa.no_rawat, reg_periksa.no_rkm_medis, reg_periksa.tgl_registrasi, case when status_lanjut = 'Ralan' then 'Rawat Jalan' else 'Rawat Inap' end as status, bridging_sep.no_sep, bridging_sep.no_kartu, bridging_sep.klsrawat, pasien.nm_pasien, pasien.tgl_lahir, pasien.jk, c.id AS id_claim, c.status_claim";
 
-            const raw = await sql(`SELECT ${fields} FROM reg_periksa LEFT JOIN pasien ON reg_periksa.no_rkm_medis = pasien.no_rkm_medis LEFT JOIN bridging_sep ON reg_periksa.no_rawat = bridging_sep.no_rawat WHERE reg_periksa.kd_pj = 'BPJ' ${tanggal} ORDER BY reg_periksa.tgl_registrasi`);
+            const raw = await sql(`SELECT ${fields} FROM reg_periksa LEFT JOIN pasien ON reg_periksa.no_rkm_medis = pasien.no_rkm_medis LEFT JOIN bridging_sep ON reg_periksa.no_rawat = bridging_sep.no_rawat LEFT JOIN idrg.claims AS c ON bridging_sep.no_sep = c.nomor_sep WHERE reg_periksa.kd_pj = 'BPJ' ${tanggal} ORDER BY reg_periksa.tgl_registrasi`);
             return { data: raw };
         }, {
         params: t.Object({ type: t.Optional(t.Union([t.Literal("rajal"), t.Literal("ranap")])) }),
--- src/post.ts
+++ src/post.ts
@@ -15,20 +15,62 @@ const forward = async (body: unknown) => {
 const post = new Elysia({ prefix: '/send' })
     .post(
         "/new-claim",
-        ({ body }) => forward(body),
-        { body: t.Object({ "metadata": t.Object({ "method": t.String() }), "data": t.Object({ "nomor_kartu": t.String(), "nomor_sep": t.String(), "nomor_rm": t.String(), "nama_pasien": t.String(), "tgl_lahir": t.String(), "gender": t.String() }) }) }
-    )
+        async ({ body }) => {
+            const res = await forward({
+                metadata: {
+                    "method": "new_claim"
+                },
+                data: body.data
+            })
+            if (res.metadata.code === 200) {
+                return res.response;
+            } else {
+                return { error: res.metadata.message };
+            }
+
+        }, {
+        body: t.Object({ "data": t.Object({ "nomor_kartu": t.String(), "nomor_sep": t.String(), "nomor_rm": t.String(), "nama_pasien": t.String(), "tgl_lahir": t.String(), "gender": t.String() }) })
+    })
 
     .post(
         "/set-claim-data",
-        ({ body }) => forward(body),
-        { body: t.Object({ "metadata": t.Object({ "method": t.String(), "nomor_sep": t.String() }), "data": t.Object({ "nomor_sep": t.String(), "nomor_kartu": t.String(), "tgl_masuk": t.String(), "tgl_pulang": t.String(), "cara_masuk": t.String(), "jenis_rawat": t.String(), "kelas_rawat": t.String(), "adl_sub_acute": t.String(), "adl_chronic": t.String(), "icu_indikator": t.String(), "icu_los": t.String(), "upgrade_class_ind": t.String(), "add_payment_pct": t.String(), "birth_weight": t.String(), "sistole": t.Number(), "diastole": t.Number(), "discharge_status": t.String(), "tarif_rs": t.Object({ "prosedur_non_bedah": t.String(), "prosedur_bedah": t.String(), "konsultasi": t.String(), "tenaga_ahli": t.String(), "keperawatan": t.String(), "penunjang": t.String(), "radiologi": t.String(), "laboratorium": t.String(), "pelayanan_darah": t.String(), "rehabilitasi": t.String(), "kamar": t.String(), "rawat_intensif": t.String(), "obat": t.String(), "obat_kronis": t.String(), "obat_kemoterapi": t.String(), "alkes": t.String(), "bmhp": t.String(), "sewa_alat": t.String() }), "pemulasaraan_jenazah": t.String(), "kantong_jenazah": t.String(), "peti_jenazah": t.String(), "plastik_erat": t.String(), "desinfektan_jenazah": t.String(), "mobil_jenazah": t.String(), "desinfektan_mobil_jenazah": t.String(), "covid19_status_cd": t.String(), "nomor_kartu_t": t.String(), "episodes": t.String(), "akses_naat": t.String(), "isoman_ind": t.String(), "bayi_lahir_status_cd": t.Number(), "dializer_single_use": t.String(), "kantong_darah": t.Number(), "alteplase_ind": t.Number(), "tarif_poli_eks": t.String(), "nama_dokter": t.String(), "kode_tarif": t.String(), "payor_id": t.String(), "payor_cd": t.String(), "cob_cd": t.Number(), "coder_nik": t.String() }) }) }
+        async ({ body }) => {
+            const res = await forward({
+                metadata: {
+                    "method": "set_claim_data",
+                    "nomor_sep": body.data.nomor_sep
+                },
+                data: body.data
+            })
+            if (res.metadata.code === 200) {
+                return res.response;
+            } else {
+                return { error: res.metadata.message };
+            }
+
+        },
+        { body: t.Object({ "data": t.Object({ "nomor_sep": t.String(), "nomor_kartu": t.String(), "tgl_masuk": t.String(), "tgl_pulang": t.String(), "cara_masuk": t.String(), "jenis_rawat": t.String(), "kelas_rawat": t.String(), "adl_sub_acute": t.String(), "adl_chronic": t.String(), "icu_indikator": t.String(), "icu_los": t.String(), "upgrade_class_ind": t.String(), "add_payment_pct": t.String(), "birth_weight": t.String(), "sistole": t.Number(), "diastole": t.Number(), "discharge_status": t.String(), "tarif_rs": t.Object({ "prosedur_non_bedah": t.String(), "prosedur_bedah": t.String(), "konsultasi": t.String(), "tenaga_ahli": t.String(), "keperawatan": t.String(), "penunjang": t.String(), "radiologi": t.String(), "laboratorium": t.String(), "pelayanan_darah": t.String(), "rehabilitasi": t.String(), "kamar": t.String(), "rawat_intensif": t.String(), "obat": t.String(), "obat_kronis": t.String(), "obat_kemoterapi": t.String(), "alkes": t.String(), "bmhp": t.String(), "sewa_alat": t.String() }), "pemulasaraan_jenazah": t.String(), "kantong_jenazah": t.String(), "peti_jenazah": t.String(), "plastik_erat": t.String(), "desinfektan_jenazah": t.String(), "mobil_jenazah": t.String(), "desinfektan_mobil_jenazah": t.String(), "covid19_status_cd": t.String(), "nomor_kartu_t": t.String(), "episodes": t.String(), "akses_naat": t.String(), "isoman_ind": t.String(), "bayi_lahir_status_cd": t.Number(), "dializer_single_use": t.String(), "kantong_darah": t.Number(), "alteplase_ind": t.Number(), "tarif_poli_eks": t.String(), "nama_dokter": t.String(), "kode_tarif": t.String(), "payor_id": t.String(), "payor_cd": t.String(), "cob_cd": t.Number(), "coder_nik": t.String() }) }) }
     )
 
     .post(
         "/idrg-diagnosa-set",
-        ({ body }) => forward(body),
-        { body: t.Object({ "metadata": t.Object({ "method": t.String(), "nomor_sep": t.String() }), "data": t.Object({ "diagnosa": t.String() }) }) }
+        async ({ body }) => {
+            const res = await forward({
+                metadata: {
+                    "method": "new_claim"
+                },
+                data: body.data
+            })
+            if (res.metadata.code === 200) {
+                return res.response;
+            } else {
+                return { error: res.metadata.message };
+            }
+
+        },
+        {
+            body: t.Object({ "nomor_sep": t.String(), "data": t.Object({ "diagnosa": t.String() }) })
+        }
     )
 
     .post(
